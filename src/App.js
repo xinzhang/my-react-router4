23c4ea6e69c4ce4b7c1ae4a8e0186096281d007b
xin zhang <xin_zhang_2000@yahoo.com>
2017-06-28 00:17:09 +1000

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import  {BrowserRouter as Router, Route, NavLink} from 'react-router-dom';
+import  {BrowserRouter as Router, Route, NavLink, Switch} from 'react-router-dom';
 import './App.css';
 
 const Home = (props) => {
